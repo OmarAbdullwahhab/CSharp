e3728cd1b41ca551e8e2c0efb18ecaa2499ae827
Omar AbdullWahhab2 <omar.abdullWahhab@outlook.com>
2020-02-07 17:35:17 +0300

    Simple Calculator Added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            var sum = new Calculator().Add(150).Add(50.78).Result;
+            Console.WriteLine("Sum = {0}", sum); 
         }
     }
 }
